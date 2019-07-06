588c588808613b8b651da82988bfb9a5638a13a7
bekowsky <bekowsky32@gmail.com>
2019-07-06 20:17:50 +0300

    test3

---
+++ TaskList/Scripts/Actions.js
@@ -0,0 +1,27 @@
+﻿function ShowDates(max, day) {
+    var i, j;
+    var days = 1;
+    for (i = 1; i < 7; i++) {
+        var str= '';
+        for (j = 1; (j < 8) && (days < max+1); j++) {
+            if ((i == 1) && (j < day))
+                str += '<td></td>'; else {
+                str += '<td><button type="button" class="btn btn-secondary btn-lg">' + days + '</button></td>';
+                days++;
+            }
+        }
+        var hui = 'str' + i;
+        var elem = document.getElementById(hui);
+        elem.innerHTML = str;
+
+    }
+ 
+}
+
+
+window.onload = function () {
+    ShowDates(31, 4);
+    $("#LayoutCalendar")
+        .parent()
+        .addClass("active");
+};
---
+++ TaskList/Scripts/CreateProjects.js
@@ -0,0 +1,29 @@
+﻿var add = document.getElementById("AddStep");
+var num = 0;
+var str = '';
+add.onclick = function () {
+    if (num != 0) {
+       
+        str += "<img  class='col-xs-offset-6' src=" + "\\" + "Images/line.png>";
+       
+    }
+    var id = "step" + num;
+    str += '<p id =' + id + ' class = "text-center">';
+
+    str += document.getElementById("InputArea").value;
+    str += '</p>';
+    
+    document.getElementById("TextArea").innerHTML = str;
+    func(id);
+    num++;
+}
+
+function func() {
+    for (var i = 0; i < num+1; i ++)
+    document.getElementById('step'+ i).style.backgroundColor = 'gainsboro';
+    
+}
+
+window.onload = function () {
+    $("#myModalBox").modal('show');
+};
